4375767569ca5fd6845dfc4a8cb0ab45c0b1666d
Ariful Islam <arifulislamat@gmail.com>
2022-10-22 16:16:51 +0600

    Working VueJS Counter App

---
+++ script.js
@@ -0,0 +1,20 @@
+// Vue code
+const vueCDN = Vue.createApp({ //Vue.createApp is a method
+    data(){
+        return {
+            heading: 'VUE CDN DEMO',
+            countValue: 0
+        }
+    }
+})
+// Mounting the newly created app (vueCDN) into a id of vueCDN (html id).
+vueCDN.mount('#vueCDN')
+
+
+// Console Message 
+info_str = " __ _\n"+
+"/ _(_)_ __ ___  _ __ __ _\n"+
+"| |_| | '__/ _ \| '__/ _` |\n"+
+"|  _| | | | (_)| | | (_| |\n"+
+"|_| |_|_|  \___/|_|   \__,_|"
+console.log(info_str)
